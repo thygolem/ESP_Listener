eabd24bff6c3abfc9861592f72b53e51966f36e3
thygolem <thygolem@gmail.com>
2021-06-17 12:46:13 +0200

    Logicas para interiores

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 a = 0
 b = None
-s3 = "{'10:00:00:00:00:02' : ['RSSI' : -56] }"
+s3 = "{'10:00:00:00:00:01' : ['RSSI' : -56] }"
 mac_address = s3[2:19]
-rssi = s3[33:36]
+rssi = int(s3[33:36])
 int(rssi)
 
 print(mac_address)
 print(rssi)
+print("- - - - - - - - - - - - - - - - - - - - - -")
 
 
 if mac_address == "10:00:00:00:00:01" :
     print('Uno está presente')
-    if rssi >= -55:
+    if rssi < -55:
         b = True
+        print(b)
 else:
-    #print('MAL')
-    a = -10
+    print('MAL \n')
 #print(a)
 #print(b)
 s3.find("RSSI")
