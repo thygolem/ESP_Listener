046b58f833935982aa863a790e51c06e64ee2457
thygolem <thygolem@gmail.com>
2021-06-23 11:03:16 +0200

    emojis added

--- main.py
+++ main.py
@@ -7,25 +7,36 @@ s3 = "{'10:00:00:00:00:01' : ['RSSI' : -56] }"
 mac_address = s3[2:19]
 rssiStr = s3[33:36]
 rssi = int(rssiStr)
-distance = rssi * -0.05
+
+rssiTrigger, distance = -57, rssi * -0.05 #estos valores cambian en f(x) de la zona que estemos, ya que BLE es sensible incluso a la humedad
+
 devicesCount = (s3.count("RSSI"))
 
-print(mac_address," has", rssi, "dBs & it's","{0:.1f}".format(distance), "meters from BLE antenna")
+print(mac_address,"is the nearest device and its RSSI LEVEL IS =", rssi, "dBs(","{0:.1f}".format(distance), "meters from BLE antenna )")
 
-if devicesCount >= 1:
-    print("HAY DISPOSITIVOS EN ESTA ZONA")
+
+print("¿AFORO MÁXIMO?")
+aforo = int(input()) # molaría que este valor lo diera un dispositivo físico con un dipswitch, sin necesidad de LCD  
+
+
+if devicesCount >= aforo:
+    print("El aforo máximo elegido es", aforo, ", ¡ ACTIVANDO EQUIPOS DE SONIDO !")
     #MQTT PUBLISH devicesCount || MONGOENGINE WRITE TO JSON COLLECTION & JS-API EXPOSES
     if mac_address == "10:00:00:00:00:01" :
-        print('UNO está presente')
-        if rssi >= -55:
+        if rssi >= rssiTrigger:
             b = True
-            print("MAC ESTÁ AQUÍ")
+            print("\U0001f600")
+            print("\U0001F916")
+            print("\U0001F9F0")
+
+
+
     else:
         print('NO HAY NÁ')
 else:
     print("listening - - - - - - - - - - -")
 
-# Para automatizar en f(macType) (macType=x), habrá que hacer un JSON aparte donde especifique el tipo de activo 
+# Para automatizar en f(macType) (macType=x), habrá que hacer un JSON aparte donde especifique el tipo de activo, cada tipo de activo llevará un emoji de identificación
 
 
 
