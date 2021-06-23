ef794dcfbae0b6ec9bb2eba3c44b27f56e4145d8
thygolem <thygolem@gmail.com>
2021-06-23 10:22:02 +0200

    Much clear

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 #import paho para usar MQTT 
 #import mongoengine para guardar en DB
-#import pandas para mapas de calor sobre imagen ?? valdría para análisis,
+#import pandas para mapas de calor sobre imagen ?? valdría para análisis visual
 
-# [ pero la representación debe de ser en tº real en javacript web + phas3r
-# [ estudiar la viabilidad de usar docker-compose.yml para ejecutar el entorno web y el script de python
-# [ El stack sería Python=Backend, JS=Frontend. Sin embargo, quizás sea interesante realizar las lógicas de activación en JS y con WebSockets
-
-a = 0
-b = None
+b = False
 s3 = "{'10:00:00:00:00:01' : ['RSSI' : -56] }"
 mac_address = s3[2:19]
 rssiStr = s3[33:36]
@@ -15,20 +10,20 @@ rssi = int(rssiStr)
 distance = rssi * -0.05
 devicesCount = (s3.count("RSSI"))
 
-print(mac_address," has", rssi, "dBs, it's","{0:.1f}".format(distance), "meters from BLE antenna")
+print(mac_address," has", rssi, "dBs & it's","{0:.1f}".format(distance), "meters from BLE antenna")
 
-if devicesCount >= 0:
-    print("ON")
+if devicesCount >= 1:
+    print("HAY DISPOSITIVOS EN ESTA ZONA")
+    #MQTT PUBLISH devicesCount || MONGOENGINE WRITE TO JSON COLLECTION & JS-API EXPOSES
     if mac_address == "10:00:00:00:00:01" :
         print('UNO está presente')
         if rssi >= -55:
             b = True
-            print(b)
+            print("MAC ESTÁ AQUÍ")
     else:
         print('NO HAY NÁ')
-    #MQTT PUBLISH
 else:
-    print("OFF")
+    print("listening - - - - - - - - - - -")
 
 # Para automatizar en f(macType) (macType=x), habrá que hacer un JSON aparte donde especifique el tipo de activo 
 
@@ -39,5 +34,8 @@ else:
 # Usar el método find(mac_addresses_saved) para hacer lógicas en función de cada una
 
 
+# [ pero la representación debe de ser en tº real en javacript web + phas3r
+# [ estudiar la viabilidad de usar docker-compose.yml para ejecutar el entorno web y el script de python
+# [ El stack sería Python=Backend, JS=Frontend. Sin embargo, quizás sea interesante realizar las lógicas de activación en JS y con WebSockets
 
 
